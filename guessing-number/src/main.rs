b196f2d12d5e2fe21ee4dcd4993935832b7f26d4
zheroindev <zheroin.dev@gmail.com>
2023-06-15 13:33:58 -0700

    number guessing

--- guessing-number/src/main.rs
+++ guessing-number/src/main.rs
@@ -1,3 +1,4 @@
+use std::io;
 fn main() {
     println!("Guess the number!");
     println!("Please input your guess.");
@@ -5,4 +6,5 @@ fn main() {
     io::stdin().read_line(&mut guess).expect("Failed to read line");
     println!("You guessed: {}", guess);
     let greeting = guess.contains("hello world");
+    println!("{}", greeting)
 }
