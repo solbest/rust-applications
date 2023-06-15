ee0a36391f55aa9c49b1594eebf756fb92a116d8
zheroindev <zheroin.dev@gmail.com>
2023-06-15 13:03:05 -0700

    guessing number

---
+++ guessing-number/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("Guess the number!");
+    println!("Please input your guess.");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    println!("You guessed: {}", guess);
+    let greeting = guess.contains("hello world");
+}
---
+++ guessing-number/target/debug/deps/guessing_number-4226ea0a4a95ae76.d
@@ -0,0 +1,5 @@
+f:\Code\learing rust\guessing-number\target\debug\deps\guessing_number-4226ea0a4a95ae76.rmeta: src\main.rs
+
+f:\Code\learing rust\guessing-number\target\debug\deps\guessing_number-4226ea0a4a95ae76.d: src\main.rs
+
+src\main.rs:
