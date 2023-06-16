3c834a219983d78c9a69ff2cd454d1dd035b4a2d
zheroindev <zheroin.dev@gmail.com>
2023-06-16 01:58:10 -0700

    info change

--- generating-number/target/debug/deps/generating_number-6bc261dab3f0abdf.d
+++ generating-number/target/debug/deps/generating_number-6bc261dab3f0abdf.d
@@ -1,5 +1,5 @@
-f:\Code\learing rust\generating-number\target\debug\deps\generating_number-6bc261dab3f0abdf.rmeta: src\main.rs
+F:\Code\learing rust\generating-number\target\debug\deps\generating_number-6bc261dab3f0abdf.rmeta: src\main.rs
 
-f:\Code\learing rust\generating-number\target\debug\deps\generating_number-6bc261dab3f0abdf.d: src\main.rs
+F:\Code\learing rust\generating-number\target\debug\deps\generating_number-6bc261dab3f0abdf.d: src\main.rs
 
 src\main.rs:
