634e16ba53f7fd1678685d9af1df1ed185218993
zheroindev <zheroin.dev@gmail.com>
2023-06-16 03:41:51 -0700

    variable use in block

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
-    let mut x = 5;
-    println!("Print number {x}");
-    x = 6;
-    println!("Update number {x}")
+    let mut x = 1;
+    println!("Print x: {x}");
+    x = 2;
+    println!("Update x: {x}");
+    let y = 3;
+    println!("Print y: {y}");
+    {
+        let y = 4;
+        println!("Print y in block: {y}");
+    }
 }
